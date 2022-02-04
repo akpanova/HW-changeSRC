a4dbda04296e7a87116ff21966da28b616b5d31f
Акмарал#2830 <a.k.akpanova@gmail.com>
2022-02-04 22:56:00 +0600

    Update index.js

--- changeSCR/index.js
+++ changeSCR/index.js
@@ -5,19 +5,19 @@ let linkOne = document.querySelector('#link-one'),
 let img = document.querySelector('img')
 
 // linkOne.addEventListener('click', () => {
-//     img.scr = 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/hbz-50-most-romantic-movies-of-all-time-titantic-1517604052.jpg';
+//     img.src = 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/hbz-50-most-romantic-movies-of-all-time-titantic-1517604052.jpg';
 // })
 
 // linkTwo.addEventListener('click', () => {
-//     img.scr = 'https://prikolnye-kartinki.ru/img/picture/Aug/28/c78389df2584386efbc79a944ffa5c5b/1.jpg';
+//     img.src = 'https://prikolnye-kartinki.ru/img/picture/Aug/28/c78389df2584386efbc79a944ffa5c5b/1.jpg';
 // })
 
 // linkThree.addEventListener('click', () => {
-//     img.scr = 'https://www.hellomagazine.com/imagenes/film/20211028123807/horror-films-garunteed-to-give-you-sleepless-nights/0-598-114/horror-t.jpg';
+//     img.src = 'https://www.hellomagazine.com/imagenes/film/20211028123807/horror-films-garunteed-to-give-you-sleepless-nights/0-598-114/horror-t.jpg';
 // })
 
 // linkFour.addEventListener('click', () => {
-//     img.scr = 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/hbz-50-most-romantic-movies-of-all-time-titantic-1517604052.jpg';
+//     img.src = 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/hbz-50-most-romantic-movies-of-all-time-titantic-1517604052.jpg';
 // })
 
 linkOne.addEventListener('click', () => {
